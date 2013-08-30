d5d21c41fbbb316f7b280f8d4b39fe6544f2f342
Nico <nicolas.jouanin@gmail.com>
2013-08-30 22:01:17 +0200

    Fix

--- cat_display_name.py
+++ cat_display_name.py
@@ -3,20 +3,20 @@
 from pelican import signals
 from pelican import contents
 
-def map_cat_diaply_name(sender, instance):
-        if hasattr(instance, 'category'):
-            category_name = instance.category.name
-            cat_name_map = instance.settings.get('CAT_DISPLAY_NAME_MAP', {})
-            try :
-                instance.category.display_name = cat_name_map[category_name]
-            except (KeyError):
-                instance.category.display_name = category_name
+def map_cat_display_name(instance):
+    if hasattr(instance, 'category'):
+        category_name = instance.category.name
+        cat_name_map = instance.settings.get('CAT_DISPLAY_NAME_MAP', {})
+        try :
+            instance.category.display_name = cat_name_map[category_name]
+        except (KeyError):
+            instance.category.display_name = category_name
 
-            cat_subtitle_map = instance.settings.get('CAT_SUBTITLE_MAP', {})
-            try :
-                instance.category.subtitle = cat_subtitle_map[category_name]
-            except (KeyError):
-                pass
+        cat_subtitle_map = instance.settings.get('CAT_SUBTITLE_MAP', {})
+        try :
+            instance.category.subtitle = cat_subtitle_map[category_name]
+        except (KeyError):
+            pass
 
 def register():
-        signals.content_object_init.connect(map_cat_diaply_name, sender=contents.Article)
+    signals.content_object_init.connect(map_cat_display_name)
