636c8af7049ab0b97ee18066870cad4b1b75013a
Nico <nicolas.jouanin@gmail.com>
2014-09-10 22:24:22 +0200

    Use absolute URLS

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,7 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 THEME="theme"
 PLUGINS = ["sitemap","thumbnail"]
